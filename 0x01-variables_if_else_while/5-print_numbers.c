e0d25179b8843e6dc5a9df5d08f6bb9422ace892
Mustapha Maleek <mustaphamaleek24@gmail.com>
2023-02-18 11:20:32 -0800

    c programs

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main -base 10 numbers
+ *
+ *Return: 0
+*/
+int main(void)
+{
+	int num;
+
+	for (num >= 0; num < 10; num++)
+	{
+		printf("%d", num);
+	}
+	printf("\n");
+return (0);
+}
