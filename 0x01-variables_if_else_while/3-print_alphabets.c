6be2065bd8e47eb10122fb80842e5bb885242573
Mustapha Maleek <mustaphamaleek24@gmail.com>
2023-02-18 10:24:41 -0800

    c programs

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ *main -upper and lower case letters
+ *
+ *Return: 0
+*/
+char main(void)
+{
+	char chu;
+	char chl;
+
+	for (chu = 'a'; chu <= 'z'; chu++)
+	{
+		putchar(chu);
+	}
+	for (chl = 'A'; chl <= 'Z'; chl++)
+	{
+		putchar(chl);
+	}
+putchar("\n");
+	return (0);
+}
+
